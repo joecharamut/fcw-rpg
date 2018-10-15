c1d2d4742ee14bba50419e8d8df0ee9b4b7c8ad8
Joseph Charamut <12399761+joecharamut@users.noreply.github.com>
2018-10-14 21:11:35 -0400

    trying to edit cmakelists whee

--- sprite.cpp
+++ sprite.cpp
@@ -1,9 +1,21 @@
-#include "sprite.h"
+#include "Sprite.h"
+
+Sprite::Sprite(float x, float y, ALLEGRO_BITMAP *image, const char *id) {
+    this->x = x;
+    this->y = y;
+    this->image = image;
+    this->width = al_get_bitmap_width(image);
+    this->height = al_get_bitmap_height(image);
+    this->id = id;
+}
 
 Sprite::Sprite(float x, float y, ALLEGRO_BITMAP *image) {
     this->x = x;
     this->y = y;
     this->image = image;
+    this->width = al_get_bitmap_width(image);
+    this->height = al_get_bitmap_height(image);
+    this->id = "s_default_id";
 }
 
 void Sprite::draw() {
