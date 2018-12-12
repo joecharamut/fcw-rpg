5791e58d0cf00ba4346c3ac96455abf9bbc6cc0f
Joseph Charamut <12399761+joecharamut@users.noreply.github.com>
2018-12-12 11:39:01 -0500

    working on making 1 map multiple rooms

--- Map.cpp
+++ Map.cpp
@@ -12,15 +12,15 @@
 static std::map<std::string, std::string> mapList = {};
 
 // Constructor
-Map::Map(std::string id, std::vector<std::string> tileset, std::vector<std::vector<std::vector<int>>> tilemap,
+Room::Room(std::string id, std::vector<std::string> tileset, std::vector<std::vector<std::vector<int>>> tilemap,
         std::vector<Sprite> sprites, std::vector<std::string> events, std::vector<Text> texts,
-        std::vector<std::string> music) {
+        std::vector<std::string> music, Map *parent) {
     // Copy in id
     this->id = std::move(id);
 
     // Copy in tileset, with path
     for (auto &str : tileset) {
-        str = Map::getFilePath(str);
+        str = Map::getFilePath(str, parent);
     }
 
     // Load the tilemap as actual images into memory
@@ -30,7 +30,7 @@ Map::Map(std::string id, std::vector<std::string> tileset, std::vector<std::vect
     for (auto spr : sprites) {
         for (auto &animation : spr.frames) {
             for (auto &frame : animation.frames) {
-                frame = Map::getFilePath(frame);
+                frame = Map::getFilePath(frame, parent);
             }
         }
         this->sprites.push_back(new Sprite(&spr));
@@ -49,8 +49,151 @@ Map::Map(std::string id, std::vector<std::string> tileset, std::vector<std::vect
 
     // Load in music
     for (const auto &file : music) {
-        this->music.push_back(al_load_sample(Map::getFilePath(file).c_str()));
+        this->music.push_back(al_load_sample(Map::getFilePath(file, parent).c_str()));
     }
+}
+
+// Function to resolve the tileset to a list of Animations with the frames loaded in memory
+std::vector<Animation *> Room::resolveTileset(std::vector<std::string> in) {
+    // Create out array
+    std::vector<Animation *> out = {};
+    // Reserve space
+    out.reserve(in.size());
+    // For each tile, create an Animation and store it
+    for (const auto &tile : in) {
+        out.push_back(new Animation(tile));
+    }
+    return out;
+}
+
+// Function to resolve the tileset and tilemap into images
+void Room::resolveMap(std::vector<std::string> tileset, std::vector<std::vector<std::vector<int>>> tilemap) {
+    // Get layers, width, and height
+    int layers = (int) tilemap.size();
+    int width = (int) tilemap[0].size();
+    int height  = (int) tilemap[0][0].size();
+
+    // Get the tileset as images
+    std::vector<Animation *> animationMap = resolveTileset(std::move(tileset));
+    // For each layer
+    for (int l = 0; l < layers; l++) {
+        // Create a new blank bitmap
+        backgrounds.push_back(al_create_bitmap(width*32, height*32));
+        // Set it as the target
+        al_set_target_bitmap(backgrounds[l]);
+        // Clear it to transparent
+        al_clear_to_color(al_map_rgba(0x00, 0x00, 0x00, 0x00));
+        // For every tile in the tilemap
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                // If it's not transparent
+                if (tilemap[l][x][y] != -1) {
+                    // Draw it to the bitmap
+                    al_draw_bitmap(animationMap[tilemap[l][x][y]]->nextFrame(), x*32, y*32, 0);
+                }
+            }
+        }
+    }
+    // Reset the target to the display
+    al_set_target_backbuffer(al_get_current_display());
+}
+
+// Function to update the viewport based on a sprite (Presumably the player)
+void Room::updateViewport(Sprite *spr, bool override) {
+    bool changed = false;
+
+    float cX = SCREEN_W/2.0f;
+    float cY = SCREEN_H/2.0f;
+    float csX = (spr->x + (spr->width/2.0f));
+    float csY = (spr->y + (spr->height/2.0f));
+    if (csX != cX || csY != cY) {
+        float dX = cX - csX;
+        float dY = cY - csY;
+        if (override) {
+            viewportX -= dX;
+            viewportY -= dY;
+            spr->setX(spr->x+dX);
+            spr->setY(spr->y+dY);
+            changed = true;
+        } else {
+            float newX = viewportX - dX;
+            float newY = viewportY - dY;
+            if (newX >= 0 && newX <= al_get_bitmap_width(backgrounds[0])-SCREEN_W) {
+                viewportX -= dX;
+                spr->setX(spr->x+dX);
+                changed = true;
+            }
+            if (newY >= 0 && newY <= al_get_bitmap_height(backgrounds[0])-SCREEN_H) {
+                viewportY -= dY;
+                spr->setY(spr->y+dY);
+                changed = true;
+            }
+        }
+        if (changed) {
+            for (auto *sprite : sprites) {
+                if (sprite->id != spr->id) {
+                    sprite->setDisplace(viewportX, viewportY);
+                }
+            }
+        }
+    }
+}
+
+// Function to draw a map to the screen
+void Room::draw() {
+    // For every background image
+    for (auto bg : backgrounds) {
+        // Draw the viewable portion of it to the screen
+        al_draw_bitmap_region(bg, viewportX, viewportY, 512, 512, 0, 0, 0);
+    }
+    // For the texts, draw them
+    // TODO: Make this event controlled. Maybe something similar to music module, but scrolling text and playing a sound
+    for (auto text : texts) {
+        al_draw_text(Main::fonts[text->font], al_map_rgb(text->r, text->g, text->b),
+                     text->x, text->y, 0, text->text.c_str());
+    }
+    // For each sprite, draw it
+    for (auto *spr : sprites) {
+        spr->draw();
+    }
+
+    for (auto *event : events) {
+        // TODO: Fix
+        event->doEvent(nullptr);
+    }
+}
+
+Sprite *Room::getSpriteById(std::string id) {
+    for (auto *spr : sprites) {
+        if (spr->id == id) {
+            return spr;
+        }
+    }
+    return nullptr;
+}
+
+Sprite* Room::checkCollision(Sprite *sprite) {
+    BoundingBox *box = sprite->boundingBox;
+    for (auto *spr : sprites) {
+        if (spr->collision == NONE) {
+            continue;
+        }
+        BoundingBox *check = spr->boundingBox;
+        if (BoundingBox::intersect(check, box)) {
+            return spr;
+        }
+    }
+    return nullptr;
+}
+
+
+
+
+
+Map::Map(std::string id, std::string defaultRoom, std::vector<std::string> roomFiles,
+         std::map<std::string, Text> textsString, std::map<std::string, std::string> soundEffectsString,
+         std::map<std::string, std::string> musicString) {
+
 }
 
 // Load map by id
@@ -74,8 +217,8 @@ Map* Map::loadMapFile(std::string filename) {
         // Load the data to the object
         inputArchive(cereal::make_nvp("mapdata", loaded));
         // Create the map with the loaded data
-        Map *m = new Map(loaded.id, loaded.tileset, loaded.tilemap,
-                loaded.sprites, loaded.events, loaded.texts, loaded.music);
+        Map *m = new Map(loaded.id, loaded.defaultRoom, loaded.roomFiles, loaded.textsString,
+                loaded.soundEffectsString, loaded.musicString);
         // Get end time
         long long int end = Util::getMilliTime();
         // Print delta
@@ -89,9 +232,9 @@ Map* Map::loadMapFile(std::string filename) {
 }
 
 // Get the path for a map file
-std::string Map::getFilePath(std::string filename) {
+std::string Map::getFilePath(std::string filename, Map *map) {
     // Get the base path
-    std::string path = mapList[id];
+    std::string path = mapList[map->id];
     std::vector<std::string> split = Util::splitString(path, "/");
     // Remove map filename from path and add the new file
     path = path.substr(0, path.length() - split.back().length()) + "data/" + filename;
@@ -132,158 +275,25 @@ std::vector<std::string> Map::enumerateMaps() {
     return maps;
 }
 
-// Function to resolve the tileset to a list of Animations with the frames loaded in memory
-std::vector<Animation *> resolveTileset(std::vector<std::string> in) {
-    // Create out array
-    std::vector<Animation *> out = {};
-    // Reserve space
-    out.reserve(in.size());
-    // For each tile, create an Animation and store it
-    for (const auto &tile : in) {
-        out.push_back(new Animation(tile));
-    }
-    return out;
+std::vector<Sprite *> Map::getSprites() {
+    return rooms[currentRoom]->sprites;
 }
 
-// Function to resolve the tileset and tilemap into images
-void Map::resolveMap(std::vector<std::string> tileset, std::vector<std::vector<std::vector<int>>> tilemap) {
-    // Get layers, width, and height
-    int layers = (int) tilemap.size();
-    int width = (int) tilemap[0].size();
-    int height  = (int) tilemap[0][0].size();
-
-    // Get the tileset as images
-    std::vector<Animation *> animationMap = resolveTileset(std::move(tileset));
-    // For each layer
-    for (int l = 0; l < layers; l++) {
-        // Create a new blank bitmap
-        backgrounds.push_back(al_create_bitmap(width*32, height*32));
-        // Set it as the target
-        al_set_target_bitmap(backgrounds[l]);
-        // Clear it to transparent
-        al_clear_to_color(al_map_rgba(0x00, 0x00, 0x00, 0x00));
-        // For every tile in the tilemap
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                // If it's not transparent
-                if (tilemap[l][x][y] != -1) {
-                    // Draw it to the bitmap
-                    al_draw_bitmap(animationMap[tilemap[l][x][y]]->nextFrame(), x*32, y*32, 0);
-                }
-            }
-        }
-    }
-    // Reset the target to the display
-    al_set_target_backbuffer(al_get_current_display());
-}
-
-// Function to draw a map to the screen
-void Map::draw() {
-    // For every background image
-    for (auto bg : backgrounds) {
-        // Draw the viewable portion of it to the screen
-        al_draw_bitmap_region(bg, viewportX, viewportY, 512, 512, 0, 0, 0);
-    }
-    // For the texts, draw them
-    // TODO: Make this event controlled. Maybe something similar to music module, but scrolling text and playing a sound
-    for (auto text : texts) {
-        al_draw_text(Main::fonts[text->font], al_map_rgb(text->r, text->g, text->b),
-                text->x, text->y, 0, text->text.c_str());
-    }
-    // For each sprite, draw it
-    for (auto *spr : sprites) {
-        spr->draw();
-    }
-
-    for (auto *event : events) {
-        event->doEvent(this);
-    }
-}
-
-
-// Function to update the viewport based on a sprite (Presumably the player)
-void Map::updateViewport(Sprite *spr, bool override) {
-    bool changed = false;
-
-    float cX = SCREEN_W/2.0f;
-    float cY = SCREEN_H/2.0f;
-    float csX = (spr->x + (spr->width/2.0f));
-    float csY = (spr->y + (spr->height/2.0f));
-    if (csX != cX || csY != cY) {
-        float dX = cX - csX;
-        float dY = cY - csY;
-        if (override) {
-            viewportX -= dX;
-            viewportY -= dY;
-            spr->setX(spr->x+dX);
-            spr->setY(spr->y+dY);
-            changed = true;
-        } else {
-            float newX = viewportX - dX;
-            float newY = viewportY - dY;
-            if (newX >= 0 && newX <= al_get_bitmap_width(backgrounds[0])-SCREEN_W) {
-                viewportX -= dX;
-                spr->setX(spr->x+dX);
-                changed = true;
-            }
-            if (newY >= 0 && newY <= al_get_bitmap_height(backgrounds[0])-SCREEN_H) {
-                viewportY -= dY;
-                spr->setY(spr->y+dY);
-                changed = true;
-            }
-        }
-        if (changed) {
-            for (auto *sprite : sprites) {
-                if (sprite->id != spr->id) {
-                    sprite->setDisplace(viewportX, viewportY);
-                }
-            }
-        }
-    }
+Sprite* Map::getSpriteById(std::string id) {
+    return rooms[currentRoom]->getSpriteById(id);
 }
 
 Sprite* Map::checkCollision(Sprite *sprite) {
-    BoundingBox *box = sprite->boundingBox;
-    for (auto *spr : sprites) {
-        if (spr->collision == NONE) {
-            continue;
-        }
-        BoundingBox *check = spr->boundingBox;
-        if (BoundingBox::intersect(check, box)) {
-            return spr;
-        }
-    }
-    return nullptr;
+    return rooms[currentRoom]->checkCollision(sprite);
 }
 
-void Map::addSprite(Sprite *sprite) {
-    sprites.push_back(sprite);
+void Map::updateViewport(Sprite *spr, bool override) {
+    rooms[currentRoom]->updateViewport(spr, override);
 }
 
-void Map::addText(std::string text, std::string font,
-        unsigned char r, unsigned char g, unsigned char b, float x, float y) {
-    auto *add = new Text();
-    add->text = std::move(text);
-    add->x = x;
-    add->y = y;
-    add->font = std::move(font);
-    add->r = r;
-    add->g = g;
-    add->b = b;
-
-    texts.push_back(add);
+void Map::draw() {
+    rooms[currentRoom]->draw();
 }
-
-Sprite *Map::getSpriteById(std::string id) {
-    for (auto *spr : sprites) {
-        if (spr->id == id) {
-            return spr;
-        }
-    }
-    return nullptr;
-}
-
-
 
 void Map::handleEvent(ALLEGRO_EVENT event) {
     if (handlerFunction) {
--- Map.h
+++ Map.h
@@ -49,82 +49,110 @@ struct Text {
     }
 };
 
-struct MapJSON {
-    int version = 1;
-    std::string id;
-    std::vector<std::vector<std::vector<int>>> tilemap;
-    std::vector<std::string> tileset;
-    std::vector<Sprite> sprites;
-    std::vector<std::string> events;
-    std::vector<Text> texts;
-    std::vector<std::string> music;
-
-    MapJSON() = default;;
-
-    MapJSON(int version, std::string id,
-            std::vector<std::vector<std::vector<int>>> tilemap, std::vector<std::string> tileset,
-            std::vector<Sprite> sprites, std::vector<std::string> events, std::vector<Text> texts,
-            std::vector<std::string>music) {
-        this->version = version;
-        this->id = std::move(id);
-        this->tilemap = std::move(tilemap);
-        this->tileset = std::move(tileset);
-        this->sprites = std::move(sprites);
-        this->events = std::move(events);
-        this->texts = std::move(texts);
-        this->music = std::move(music);
-    }
-
-    template <class Archive>
-    void serialize(Archive &archive) {
-        archive(
-                CEREAL_NVP(version),
-                CEREAL_NVP(id),
-                CEREAL_NVP(tilemap),
-                CEREAL_NVP(tileset),
-                CEREAL_NVP(sprites),
-                CEREAL_NVP(events),
-                CEREAL_NVP(texts),
-                CEREAL_NVP(music)
-        );
-    }
-};
-
-class Map {
+class Room {
 public:
     std::string id;
     std::vector<ALLEGRO_BITMAP *> backgrounds = {};
     std::vector<Sprite *> sprites = {};
     std::vector<Text *> texts = {};
     std::vector<ALLEGRO_SAMPLE *> music = {};
-
     std::vector<Event *> events = {};
-
     float viewportX = 0;
     float viewportY = 0;
+
+    Room(std::string id, std::vector<std::string> tileset, std::vector<std::vector<std::vector<int>>> tilemap,
+        std::vector<Sprite> sprites, std::vector<std::string> events, std::vector<Text> texts,
+        std::vector<std::string> music, Map *parent);
+
+    std::vector<Animation *> resolveTileset(std::vector<std::string> in);
+    void resolveMap(std::vector<std::string> tileset, std::vector<std::vector<std::vector<int>>> tilemap);
+    void updateViewport(Sprite *spr, bool override);
+    Sprite* getSpriteById(std::string id);
+    Sprite* checkCollision(Sprite *sprite);
+    void draw();
+
+    struct RoomJSON {
+        int version = 1;
+        std::string id;
+        std::vector<std::vector<std::vector<int>>> tilemap;
+        std::vector<std::string> tileset;
+        std::vector<Sprite> sprites;
+        std::vector<std::string> events;
+        std::vector<Text> texts;
+        std::vector<std::string> music;
+
+        template <class Archive>
+        void serialize(Archive &archive) {
+            archive(
+                    CEREAL_NVP(version),
+                    CEREAL_NVP(id),
+                    CEREAL_NVP(tilemap),
+                    CEREAL_NVP(tileset),
+                    CEREAL_NVP(sprites),
+                    CEREAL_NVP(events),
+                    CEREAL_NVP(texts),
+                    CEREAL_NVP(music)
+            );
+        }
+    };
+};
+
+class Map {
+public:
+    bool loaded = false;
+
+    std::string id;
+    std::string defaultRoom;
+    std::map<std::string, Room *> rooms;
+    std::map<std::string, Text *> texts;
+    std::map<std::string, ALLEGRO_SAMPLE_INSTANCE *> soundEffects;
+    std::map<std::string, ALLEGRO_SAMPLE_INSTANCE *> music;
+    std::string currentRoom;
 
     void (*handlerFunction)(ALLEGRO_EVENT event);
 
     static Map* loadMap(std::string id);
     static Map* loadMapFile(std::string filename);
-    Map(std::string id, std::vector<std::string> tileset, std::vector<std::vector<std::vector<int>>> tilemap,
-            std::vector<Sprite> sprites, std::vector<std::string> events, std::vector<Text> texts,
-            std::vector<std::string> music);
     static std::vector<std::string> enumerateMaps();
+    static std::string getFilePath(std::string filename, Map *map);
 
-    std::string getFilePath(std::string filename);
-    void resolveMap(std::vector<std::string> tileset, std::vector<std::vector<std::vector<int>>> tilemap);
-    void draw();
-
-    void addSprite(Sprite *sprite);
-    void addText(std::string text, std::string font,
-            unsigned char r, unsigned char g, unsigned char b, float x, float y);
-    Sprite* getSpriteById(std::string id);
     void handleEvent(ALLEGRO_EVENT event);
     void setEventHandlerFunction(void (*handler)(ALLEGRO_EVENT event));
+
+
+    // Pass to current room
+    std::vector<Sprite *> getSprites();
+    Sprite* getSpriteById(std::string id);
     Sprite* checkCollision(Sprite *sprite);
     void updateViewport(Sprite *spr, bool override);
+    void draw();
 
+    Map(std::string id, std::string defaultRoom, std::vector<std::string> roomFiles,
+            std::map<std::string, Text> textsString, std::map<std::string, std::string> soundEffectsString,
+            std::map<std::string, std::string> musicString);
+
+    struct MapJSON {
+        int version = 1;
+        std::string id;
+        std::string defaultRoom;
+        std::vector<std::string> roomFiles;
+        std::map<std::string, Text> textsString;
+        std::map<std::string, std::string> soundEffectsString;
+        std::map<std::string, std::string> musicString;
+
+        template<class Archive>
+        void serialize(Archive &archive) {
+            archive(
+                    CEREAL_NVP(version),
+                    CEREAL_NVP(id),
+                    CEREAL_NVP(defaultRoom),
+                    CEREAL_NVP(roomFiles),
+                    cereal::make_nvp("texts", textsString),
+                    cereal::make_nvp("soundEffects", soundEffectsString),
+                    cereal::make_nvp("music", musicString)
+            );
+        }
+    };
 };
 
 #endif //FCWRPG_MAP_H
